c66682d988b7f3df0facd8cca319b67639ddc3d9
malow <134291030+MTSmalow@users.noreply.github.com>
2023-10-25 21:32:02 -0300

    Add files via upload

---
+++ suplementar/index.js
@@ -0,0 +1,26 @@
+// faz a troca das classes do tema
+const $html = document.querySelector("html");
+const $checkbox = document.querySelector("#switch");
+$checkbox.addEventListener("change", function () {
+  $html.classList.toggle("Clear-Modus");
+});
+
+/* botao hanbunger */
+
+const btnMobile = document.getElementById("btn-mobile");
+
+function toggleMenu(event) {
+  if (event.type === "touchstart") event.preventDefault();
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("active");
+  const active = nav.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", active);
+  if (active) {
+    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+  }
+}
+
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);

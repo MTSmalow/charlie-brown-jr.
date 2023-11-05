c21535252e0dca816120bcc1d22f6efda4cb89f5
malow <134291030+MTSmalow@users.noreply.github.com>
2023-11-05 12:28:16 -0300

    adiciona a função de salva a escolha do tema ao navegador

--- suplementar/index.js
+++ suplementar/index.js
@@ -1,9 +1,21 @@
 // faz a troca das classes do tema
 const $html = document.querySelector("html");
 const $checkbox = document.querySelector("#switch");
-$checkbox.addEventListener("change", function () {
+// Função para alternar o modo claro/escuro e salvar o estado no localStorage
+function toggleClearMode() {
   $html.classList.toggle("Clear-Modus");
-});
+  // Armazena o estado do checkbox no localStorage
+  localStorage.setItem("clearModeEnabled", $checkbox.checked);
+}
+// Adiciona um ouvinte de evento de mudança ao checkbox
+$checkbox.addEventListener("change", toggleClearMode);
+// Verifica o valor no localStorage e aplica-o ao carregar a página
+if (localStorage.getItem("clearModeEnabled") === "true") {
+  $checkbox.checked = true;
+  $html.classList.add("Clear-Modus");
+}
+// Chama a função para aplicar o modo claro/escuro
+toggleClearMode();
 
 /* botao hanbunger */
 
